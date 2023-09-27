e1af173ef0e2490c3563c0f3e1bc9d4442492fdd
AmanSingh44 <aman7474362@gmail.com>
2023-09-27 22:02:50 +0545

    Frontend routing for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import Signup from "./components/auth/Signup";
 import Navbar from "./components/user/Navbar";
+import Home from "./components/Home";
+import EmailVerification from "./components/auth/EmailVerification";
+import ForgetPassword from "./components/auth/ForgetPassword";
+import ConfirmPassword from "./components/auth/ConfirmPassword";
 
 function App() {
     return ( <
@@ -6,6 +13,30 @@ function App() {
         <
         Navbar / >
         <
+        Routes >
+        <
+        Route path = '/'
+        element = { < Home / > }
+        /> <
+        Route path = '/auth/signin'
+        element = { < SignIn / > }
+        /> <
+        Route path = '/auth/signup'
+        element = { < Signup / > }
+        /> <
+        Route path = '/auth/verification'
+        element = { < EmailVerification / > }
+        /> <
+        Route path = '/auth/forget-password'
+        element = { < ForgetPassword / > }
+        /> <
+        Route path = '/auth/confirm-password'
+        element = { < ConfirmPassword / > }
+        />
+
+
+        <
+        /Routes> <
         /div>
     );
 }
---
+++ frontend/src/components/auth/SignIn.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from '../Container';
+import Title from '../form/Title';
+import FormInput from '../form/FormInput';
+import Submit from '../form/Submit';
+import { Link } from 'react-router-dom';
+
+export default function SignIn() {
+  return (
+    <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+      <Container className="max-w-screen-xl mx-auto">
+        <form className='bg-secondary rounded p-6 w-72 space-y-6'>
+          <Title>Sign in</Title>
+          <FormInput label='Email' placeholder='john@email.com' name='email' />
+          <FormInput label='Password' placeholder='********' name='password' />
+          <Submit value="Sign in"/>
+
+          <div className="flex justify-between">
+            <Link className='text-dark-subtle hover:text-white transition' to='/auth/forget-password'>Forgot Password</Link>
+            <Link className='text-dark-subtle hover:text-white transition' to='/auth/signup'>Sign up</Link>
+
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
+}
+ 
